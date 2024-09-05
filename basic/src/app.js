6b3f67db1e84696e5e4c0d6b10ca66f09c710695
legcy <legcy143@gmail.com>
2024-09-06 00:52:59 +0530

    load: css in server

--- basic/src/app.js
+++ basic/src/app.js
@@ -1,3 +1,5 @@
+import "./style.css"
 console.log("lol this is from src/index default")
 console.log("lol this is from src/app")
 console.log("from webpack server")
+console.log("style integrated")
--- basic/webpack.config.js
+++ basic/webpack.config.js
@@ -3,6 +3,14 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/app.js",
+  module:{
+    defaultRules:[
+      {
+        test:/\.css$/,
+        use:["style-loader" , "css-loader"]
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "output.js",
